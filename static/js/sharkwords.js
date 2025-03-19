ab8e1499d2d33fba76b24ac6a7a988dfb21e339e
Luis Minaya <luis.minaya@gmail.com>
2025-03-18 22:08:08 -0400

    Finished sharkwords UI

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -32,12 +32,24 @@ const word = 'hello';
 //
 function createDivsForChars() {
   // Replace this with your code
+  const wordContainer = document.querySelector('#word-container')
+
+  for (const letter of word) {
+      wordContainer.insertAdjacentHTML('beforeend', 
+        `<div class="letter-box ${letter}"></div>`);      
+  }
 }
 
 // Loop over each letter in the alphabet and generate a button for each letter
 // The buttons should be appended to the section with id="letter-buttons".
 function generateLetterButtons() {
   // Replace this with your code
+  const letterButtons = document.querySelector('#letter-buttons')
+
+  for (const letter of ALPHABET) {
+    letterButtons.insertAdjacentHTML('beforeend', `<button>${letter}</button`)
+  }
+
 }
 
 // Set the `disabled` property of `buttonEl` to true.
@@ -46,6 +58,8 @@ function generateLetterButtons() {
 //
 function disableLetterButton(buttonEl) {
   // Replace this with your code
+  buttonEl.setAttribute('disabled', 'true')
+
 }
 
 // This is a helper function we will use in the future
@@ -53,6 +67,12 @@ function disableLetterButton(buttonEl) {
 // For now, you should test it out to make sure it works
 function isLetterInWord(letter) {
   // Replace this with your code
+  if (word.includes(letter)) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 // This function is called to start the game.
